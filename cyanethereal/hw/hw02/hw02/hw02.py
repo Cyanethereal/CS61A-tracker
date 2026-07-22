13b1d486d069f815c14a116d0951fe560827d911
Cyanethereal <halfawakeasleep@163.com>
2026-07-22 18:14:02 +0800

    hw02完成

--- cyanethereal/hw/hw02/hw02/hw02.py
+++ cyanethereal/hw/hw02/hw02/hw02.py
@@ -32,7 +32,10 @@ def product(n, term):
     162
     """
     "*** YOUR CODE HERE ***"
-
+    sum = 1
+    for i in range (1 ,n+1):
+        sum = sum * term(i)
+    return sum
 
 def accumulate(fuse, start, n, term):
     """Return the result of fusing together the first n terms in a sequence 
@@ -54,7 +57,10 @@ def accumulate(fuse, start, n, term):
     19
     """
     "*** YOUR CODE HERE ***"
-
+    sum = start
+    for i in range (1 ,n+1):
+        sum = fuse(sum, term(i))
+    return sum
 
 def summation_using_accumulate(n, term):
     """Returns the sum: term(1) + ... + term(n), using accumulate.
@@ -68,8 +74,7 @@ def summation_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
-
+    return accumulate(add, 0, n, term)
 
 def product_using_accumulate(n, term):
     """Returns the product: term(1) * ... * term(n), using accumulate.
@@ -83,7 +88,7 @@ def product_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
+    return accumulate(mul, 1, n, term)
 
 
 def make_repeater(f, n):
@@ -100,4 +105,11 @@ def make_repeater(f, n):
     390625
     """
     "*** YOUR CODE HERE ***"
+    def g(x):
+        result = x
+        for _ in range (n):
+            result = f(result)
+        return result
+    return g
+
 
